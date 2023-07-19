f5656169ee0b82df2c1dffbce34653fce6c16d96
Diana Pena <dianaligiapena@gmail.com>
2023-07-19 05:31:28 +0300

    list + item components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
       setTodos(currentTodos => {
@@ -36,29 +46,7 @@ export default function App() {
     <>
       <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">To-do List</h1>
-
-      <ul className="list">
-
-        {todos.length === 0 && "No Todos"} 
-
-        {todos.map(todo => {
-          return  (
-              <li key={todo.id}>
-                <label>
-                  <input 
-                    type="checkbox"  
-                    checked={todo.completed} 
-                    onChange={e => toggleTodo(todo.id, e.target.checked)}
-                    />
-                  {todo.title}
-                </label>
-                <button 
-                    onClick={() => deleteTodo(todo.id)}
-                    className="btn btn-danger">Delete</button>
-              </li>
-          )
-        })}
-      </ul>
-  </>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
